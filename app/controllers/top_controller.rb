851525a18cec96552bd00ceaa948e00904e9a491
kousaku1122 <new_address_for_kou1122saku@gmail.com>
2020-12-02 19:37:08 +0000

    uni_num

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
     def index
         if params[:uni_num].present?
-            @players = Player.where('uni_num LIKE ?', params[:uni_num])
+            @players = Player.where('uni_num LIKE ?', params[:uni_num].to_i)
         else
             @players = Player.all.order(uni_num: "ASC")
         end
